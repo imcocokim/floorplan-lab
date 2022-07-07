5cff1c1b420482a03a375140e1cf5cc70182f861
imcoco <imcoco@gmail.com>
2022-07-07 07:19:56 -0400

    add Sink

--- src/Kitchen.jsx
+++ src/Kitchen.jsx
@@ -1,10 +1,12 @@
 import Oven from "./Oven";
+import Sink from "./Sink";
 
 const Kitchen = () => {
   return ( 
     <div>
       <h1>Kitchen</h1>
       <Oven />
+      <Sink />
     </div>
   );
 }
